143e3db71c3dd62c20ade98c68983aac3e0c70a4
Joseph Estes <54252110+josephestes@users.noreply.github.com>
2022-11-18 13:27:23 -0500

    Add project files.

---
+++ Joseph.Estes.Ch3.You.Do.It/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/* 
+ * Program by Joseph Estes
+ * This program demonstrates buttons and labels in a form.
+ * 
+ * User selects the "click here" button and that displays a label,
+ * and enables a "click me last" button. This second button changes the label.
+ * 
+ */
+
+namespace Joseph.Estes.Ch3.You.Do.It
+{
+    public partial class HelloForm : Form
+    {
+        public HelloForm()
+        {
+            InitializeComponent();
+        }
+
+        private void DisplayOutputButton_Click(object sender, EventArgs e)
+        {
+            helloLabel.Visible = true;
+
+            changeOutputButton.Enabled = true;
+        }
+
+        private void ChangeOutputButton_Click(object sender, EventArgs e)
+        {
+            helloLabel.Text = "Goodbye";
+        }
+    }
+}
